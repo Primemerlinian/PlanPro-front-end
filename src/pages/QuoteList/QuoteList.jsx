be7cdca25c29f3787efe592055e17741457dd623
Primemerlinian <mwatson0518@gmail.com>
2023-02-07 23:24:41 -0500

    stub up QuoteList component in pages

---
+++ src/pages/QuoteList/QuoteList.jsx
@@ -0,0 +1,11 @@
+import styles from './QuoteList.module.css'
+
+const QuoteList = (props) => {
+  return (
+    <main className={styles.container}>
+      Quote List
+    </main>
+  )
+}
+
+export default QuoteList
